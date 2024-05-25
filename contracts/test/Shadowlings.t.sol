acb9704483d8756203c772ec99babfcc74b53af2
Nicholas Rodrigues Lordello <nick@safe.global>
2024-05-25 14:19:12 +0200

    refactor testing

--- contracts/test/Shadowlings.t.sol
+++ contracts/test/Shadowlings.t.sol
@@ -33,17 +33,13 @@ contract ShadowlingsTest is Test {
     }
 
     function test_UserOperation() public {
-        uint256 commit = uint256(uint248(uint256(keccak256("commit"))));
+        (uint256 commit, uint256 nullifier, bytes32 executionHash, Shadowlings.Proof memory proof) = _sampleProof();
+
         address token = address(0);
         address to = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
         uint256 amount = 1 ether;
 
         bytes memory callData = abi.encodeCall(shadowlings.execute, (commit, token, to, amount));
-
-        // intentionally invalid.
-        Shadowlings.Proof memory proof;
-        bytes32 nullifier = bytes32(0);
-
         bytes memory signature = abi.encode(nullifier, proof);
 
         PackedUserOperation memory userOp = PackedUserOperation({
@@ -57,7 +53,7 @@ contract ShadowlingsTest is Test {
             paymasterAndData: "",
             signature: signature
         });
-        bytes32 userOpHash = this.hashUserOp(userOp);
+        bytes32 userOpHash = executionHash;
 
         address shadowling = shadowlings.getShadowling(commit);
         vm.deal(shadowling, amount);
@@ -66,8 +62,7 @@ contract ShadowlingsTest is Test {
         assertEq(shadowling.balance, 1 ether);
 
         vm.startPrank(entryPoint);
-        // proof is known to be invalid!
-        require(shadowlings.validateUserOp(userOp, userOpHash, 0) == 1);
+        require(shadowlings.validateUserOp(userOp, userOpHash, 0) == 0);
         shadowlings.execute(commit, token, to, amount);
 
         assertEq(to.balance, 1 ether);
@@ -75,11 +70,22 @@ contract ShadowlingsTest is Test {
     }
 
     function test_VerifyProof() public {
-        uint256 commit = 0x153c333c4856f04f11c983484a8fbcd2705b4460498f55b4771cd09af3c306ab;
-        uint256 nullifier = 0x05476bcdcaba1d11916a4f3618d499f9b6c53506cb825926f25ea37e0627cc0d;
-        bytes32 executionHash = 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee;
+        (uint256 commit, uint256 nullifier, bytes32 executionHash, Shadowlings.Proof memory proof) = _sampleProof();
 
-        Shadowlings.Proof memory proof;
+        bool success = shadowlings.verifyProof(commit, nullifier, executionHash, proof);
+
+        assertTrue(success);
+    }
+
+    function _sampleProof()
+        internal
+        pure
+        returns (uint256 commit, uint256 nullifier, bytes32 executionHash, Shadowlings.Proof memory proof)
+    {
+        commit = 0x153c333c4856f04f11c983484a8fbcd2705b4460498f55b4771cd09af3c306ab;
+        nullifier = 0x05476bcdcaba1d11916a4f3618d499f9b6c53506cb825926f25ea37e0627cc0d;
+        executionHash = 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee;
+
         proof.a = Pairing.G1Point(
             0x157d71d26866fa47011e546ba75662a5c1b970c4d04955257e79a75afcfffb8c,
             0x02fad2fa9c9497cddbe199f036a41bb190b4ec18730937b649403b363ced57c1
@@ -98,13 +104,5 @@ contract ShadowlingsTest is Test {
             0x09bc3e2213f740bcdf4a3a4d9daca2acb4ebac5d610e056e233d94bff4c87f7e,
             0x1f6f09d484887497cbea24c0bb5ae662905596690c31fbcf0a3793cf2f601f04
         );
-
-        bool success = shadowlings.verifyProof(commit, nullifier, executionHash, proof);
-
-        assertTrue(success);
-    }
-
-    function hashUserOp(PackedUserOperation calldata userOp) external pure returns (bytes32 hash) {
-        hash = UserOperationLib.hash(userOp);
     }
 }
